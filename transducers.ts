8d0a0964897ad15f1da613508807f77e1f5e13f9
Magnus Kulke <mkulke@gmail.com>
2018-03-17 14:03:05 +0100

    Added typed transducers.

--- transducers.ts
+++ transducers.ts
@@ -10,6 +10,20 @@ function filter<T>(predicate: (t: T) => boolean): FilterReducer<T> {
   };
 }
 
+// return fn returning reducer
+type Transducer<T, V> = (r: Reducer<V, T>) => Reducer<V, T>;
+
+function filterT<T, V>(predicate: (t: T) => boolean): Transducer<T, V> {
+  return function(reducer: Reducer<V, T>): Reducer<V, T> {
+    return function(accumulator, value) {
+      if (predicate(value)) {
+        return reducer(accumulator, value);
+      }
+      return accumulator;
+    };
+  };
+}
+
 type MapReducer<T, V> = Reducer<Array<T>, V>;
 function map<T, V>(transformer: (v: V) => T): MapReducer<T, V> {
   // return function(accumulator: Array<T>, value: V): Array<T> {
@@ -19,9 +33,27 @@ function map<T, V>(transformer: (v: V) => T): MapReducer<T, V> {
   };
 }
 
+function mapT<T, V>(
+  transformer: (v: V) => T,
+): (r: Reducer<Array<V>, T>) => Reducer<Array<V>, V> {
+  return function(reducer: Reducer<Array<V>, T>): Reducer<Array<V>, V> {
+    return function(accumulator: Array<V>, value: V): Array<V> {
+      const transformed = transformer(value);
+      return reducer(accumulator, transformed);
+    };
+  };
+}
+
 const numbers = [1, 2, 3, 4];
-const strings = numbers
+const reduced = numbers
   .reduce(filter(v => v > 2), [])
   .reduce(map(v => v.toString()), []);
 
-console.log('mgns %j', strings);
+console.log('reduced %j', reduced);
+
+const identity = map(x => x);
+const filterX = filterT(v => v > 1);
+const mapX = mapT(v => v.toString());
+const transduced = numbers.reduce(filterX(mapX(identity)), []);
+
+console.log('transduced %j', transduced);
